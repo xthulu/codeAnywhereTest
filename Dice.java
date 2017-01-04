494cdc0c9680402665b4257e3e11518b6b5f74b9
x-codeanywhere <x-codeanywhere@bwahaha.net>
2017-01-04 13:05:05 -0500

    Risus Dice

---
+++ Dice.java
@@ -0,0 +1,11 @@
+public class Dice {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World 1");
+        System.out.println("This is the End");
+        System.out.println("All is Lost");
+        System.out.println("The future is unwritten"); 
+    }
+
+}
